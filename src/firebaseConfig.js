40f18ef91a83daddbc42a7bbb893b7e580fa6464
fdfdsfadf <Hu_Gaming1@outlook.com>
2025-10-26 18:49:57 +1100

    Create firebaseConfig.js

---
+++ src/firebaseConfig.js
@@ -0,0 +1,16 @@
+// Replace the object below with your Firebase config from the Firebase Console
+// Example:
+// export const firebaseConfig = {
+// apiKey: "...",
+// authDomain: "...",
+// databaseURL: "https://your-project-default-rtdb.firebaseio.com",
+// projectId: "...",
+// storageBucket: "...",
+// messagingSenderId: "...",
+// appId: "..."
+// };
+
+
+export const firebaseConfig = {
+  // PLACEHOLDER — replace with your config
+};
