c2eb6096a35598e0451afef31b31cb939adf2dbd
fdfdsfadf <Hu_Gaming1@outlook.com>
2025-10-26 19:45:30 +1100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// Change base to '/your-repo-name/' when publishing under a repo path
 export default defineConfig({
   plugins: [react()],
   base: './'
